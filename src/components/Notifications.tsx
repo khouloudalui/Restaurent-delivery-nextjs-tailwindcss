47749092136aece110d4797e287a77162908b3e7
kiki <aloui.khouloud11@gmail.com>
2023-11-02 12:42:40 +0100

    style:Improve notification bar appearance

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Notifications = () => {
   return (
-    <div>
-      notifications
+    <div className='bg-red-500 h-9 text-white text-sm py-6 px-4 flex justify-center items-center text-center md:text-base cursor-pointer'>
+      notificatio nsnotific ationsnotif icationsno tificationsn otifications
     </div>
   )
 }
