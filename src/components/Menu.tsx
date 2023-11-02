1d9d92b750340265b0742034e03365ac507d026a
kiki <aloui.khouloud11@gmail.com>
2023-11-02 13:10:08 +0100

    style: add link and menu compnent in navbar component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <div>
+            <Image src="/open.png" alt="menu icon" width={50} height={50}/>
+        </div>
+    )
+}
+
+export default Menu
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
+import Menu from './Menu'
 
 const Navbar = () => {
     return (
-        <div>
-            navbar
+        <div className='flex justify-between items-center  h-12 text-red-500 p-4 border-b-2 border-b-red-500 '>
+            <div>
+                <Link href="/">Fasty Pasty</Link>
+            </div>
+            <div>
+                <Menu />
+            </div>
         </div>
     )
 }
