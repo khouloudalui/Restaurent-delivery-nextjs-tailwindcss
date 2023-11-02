75e9e7544708d648959f23efefb9eac6a2394ca6
kiki <aloui.khouloud11@gmail.com>
2023-11-02 15:39:40 +0100

    feat: Add cartIcon component have a link and image

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
+import CartIcon from './CartIcon'
 
 const links = [
     { id: 1, title: "Home", url: "/" },
@@ -13,11 +14,15 @@ const links = [
 
 const Menu = () => {
     const [open, setOpen] = useState(false)
+    const user = true;
     return (
         <div >
             <Image src={open ? "/close.png" : "/open.png"} alt="menu icon" width={20} height={20} onClick={() => setOpen(!open)} />
             <div className='w-full flex flex-col bg-red-500 text-white  absolute left-0 top-24 h-[calc(100vh-6rem)] gap-8 text-2xl p-4 z-10 items-center justify-center'>
                 {links.map(item => (<Link href={item.url} key={item.id}>{item.title}</Link>))}
+                {user ? <Link href="/login">Login</Link>
+                    : <Link href="/orders">Orders</Link>}
+                <Link href="/cart"><CartIcon /></Link>
             </div>
 
         </div>
