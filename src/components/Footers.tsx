5f38af9306bcd9a7f4ae05e56f98633d664c2625
kiki <aloui.khouloud11@gmail.com>
2023-11-03 17:14:41 +0100

    feat(Footer):add footer component

--- src/components/Footers.tsx
+++ src/components/Footers.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footers = () => {
     return (
-        <div>
-            footer
+        <div className='flex justify-between items-center text-red-500 md:h-24 p-10 lg:p-20'>
+            <Link href="/" className='font-semibold text-xl'>Fasty PAsty</Link>
+            <p>ALL RIGHTS RESERVED.</p>
         </div>
     )
 }
